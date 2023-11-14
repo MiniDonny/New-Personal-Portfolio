e4ec3a552cd3fa44e78c0ca44817b643a15545e0
MiniDonny <donatorobledo@hotmail.com>
2023-11-14 14:01:11 -0300

    Advance

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-
 /*SCROLL SECTIONS*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a')
@@ -21,13 +20,22 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
+            /*active sections for animation on scroll*/
+            sec.classList.add('show-animate');
         }
-    })
+        /*if want to use animation that repeats on scrool use*/
+        else{
+            sec.classList.remove('show-animate');
+        }
+    });
     /*Sticky Header*/
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
     /*Remove toggle icon and navbar when click navbar links(scroll)*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+    /*animation footer on scroll*/
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
