86d8612116ac3f96c0f3d6ee7fa779407e5a9e7b
MiniDonny <donatorobledo@hotmail.com>
2023-11-20 19:12:52 -0300

    All Finished

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+/*MODIFY HTML*/
 document.addEventListener('DOMContentLoaded', () => {
     animacionMenuIcon();
     animationScroll();
-    header();
-    footer();
-    animacionGsap();
 });
 
 /*TOGGLE ICON NAVBAR*/
@@ -27,22 +25,13 @@ let animationScroll = window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
-            /*Active navbar links*/
+            /*ACTIVE NAVBAR LINKS*/
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-
-            /*active sections for animation on scroll*/
-            sec.classList.add('show-animate');
-        }
-
-        /*if want to use animation that repeats on scrool use*/
-        else{
-            sec.classList.remove('show-animate');
         }
     });
-
     /*Sticky Header*/
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
@@ -50,10 +39,6 @@ let animationScroll = window.onscroll = () => {
     /*Remove toggle icon and navbar when click navbar links(scroll)*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-
-    /*animation footer on scroll*/
-    let footer = document.querySelector('footer');
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 
 /* Configura las animaciones con GSAP
